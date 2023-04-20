fed2be43f30f3c09d2863161f728996657c9b2e9
Kelly Wong <kellykl_wong@hotmail.com>
2023-04-20 18:02:08 +0100

    adds Passenger Class

--- airline_api/src/main/java/com/example/airline_api/models/Flight.java
+++ airline_api/src/main/java/com/example/airline_api/models/Flight.java
@@ -1,4 +1,10 @@
 package com.example.airline_api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "flights")
 public class Flight {
+
 }
--- airline_api/src/main/java/com/example/airline_api/models/Passenger.java
+++ airline_api/src/main/java/com/example/airline_api/models/Passenger.java
@@ -1,4 +1,66 @@
 package com.example.airline_api.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "passengers")
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    private List<Flight> flights;
+
+    public Passenger(String name, int phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.flights = new ArrayList<>();
+    }
+
+    public Passenger(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
 }
