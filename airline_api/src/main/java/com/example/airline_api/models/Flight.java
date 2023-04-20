b838dd03a8d8e7f02f9382ce9d53a4dbf8e2bfb2
Kelly Wong <kellykl_wong@hotmail.com>
2023-04-20 18:24:18 +0100

    adds flight class

--- airline_api/src/main/java/com/example/airline_api/models/Flight.java
+++ airline_api/src/main/java/com/example/airline_api/models/Flight.java
@@ -1,13 +1,75 @@
 package com.example.airline_api.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "flights")
 public class Flight {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
+
+    @Column
     private String destination;
-    private 
+
+    @Column
+    private int capacity;
+
+    @Column(name = "departure_date")
+    private String departureDate;
+
+    @Column(name = "departure_time")
+    private String departureTime;
+
+    public Flight(String destination, String departureDate, String departureTime){
+        this.destination = destination;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+    }
+
+    public Flight(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
 }
